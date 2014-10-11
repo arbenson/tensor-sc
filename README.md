811f10557c52e3357db9348a279d95769565e8bd
Austin Benson <arbenson@gmail.com>
2014-10-11 12:27:59 -0700

    Update layered flow

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ tensor-sc
 =========
 
 Tensor spectral clustering.
+
+
+Getting started
+=========
+
+What you need:
+* Compiler supporting C++11
+* [SNAP](http://snap.stanford.edu/snap/index.html) for C++
+
+Edit the Makefile for your compiler and the location of SNAP.
+
+Layered flow example
+=========
+
+First, build the example:
+
+    make layered_flow
+
+Directed 3-cycle example
--- layered_flow.cpp
+++ layered_flow.cpp
@@ -9,14 +9,14 @@ int main(int argc, char **argv) {
     int num_comms = 3;
     int triple_type = D3CS_RECIP;
     int cut_type = D3C_COND_RECIP;
-    std::string name = "large_flow";
+    std::string name = "layered_flow";
     std::cout << "DIRLAP ---------" << std::endl;
     {
 	Network net = Network("data", name, triple_type);
 	CommDetection comm(net, num_comms, DIRLAP, cut_type, name);
 	comm.Run();
 	std::vector<int> comms = comm.Communities();
-	WriteVector(comms, "large_flow_dl_comms.txt");
+	WriteVector(comms, "layered_flow_dl_comms.txt");
     }
     std::cout << "D3C ONLY RECIP ---------" << std::endl;
     {
@@ -24,15 +24,15 @@ int main(int argc, char **argv) {
 	CommDetection comm(net, num_comms, D3C_ONLY_RECIP_DIRLAP, cut_type, name);
 	comm.Run();
 	std::vector<int> comms = comm.Communities();
-	WriteVector(comms, "large_flow_subdl_comms.txt");
+	WriteVector(comms, "layered_flow_subdl_comms.txt");
     }
-    std::cout << "MULTILINEAR ---------" << std::endl;
+    std::cout << "TENSOR ---------" << std::endl;
     {
 	Network net = Network("data", name, triple_type);
 	CommDetection comm(net, num_comms, MULTILINEAR, cut_type, name);
 	comm.Run();
 	std::vector<int> comms = comm.Communities();
-	WriteVector(comms, "large_flow_subdl_comms.txt");
+	WriteVector(comms, "layered_flow_tsc_comms.txt");
     }
     std::cout << "END ---------" << std::endl;
 }
