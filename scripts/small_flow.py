9123205fa898cc96f36711f64f865ac1f918f6b3
Austin Benson <arbenson@gmail.com>
2014-10-11 16:27:57 -0700

    Tweaks to small flow example

--- scripts/small_flow.py
+++ scripts/small_flow.py
@@ -17,13 +17,13 @@ G.AddEdge(3, 4)
 G.AddEdge(4, 5)
 G.AddEdge(5, 3)
 
-G.AddEdge(1, 3)
+#G.AddEdge(1, 3)
 G.AddEdge(1, 4)
 G.AddEdge(2, 3)
 G.AddEdge(2, 4)
 
 G.AddEdge(5, 0)
 
-out_file = '../data/flow.txt'
+out_file = '../data/small_flow.txt'
 print 'data:', out_file
 snap.SaveEdgeList(G, out_file)
--- small_flow.cpp
+++ small_flow.cpp
@@ -1,5 +1,6 @@
 #include "common.hpp"
 #include "community.hpp"
+#include "file_io.hpp"
 #include "network.hpp"
 
 #include <iostream>
@@ -9,25 +10,23 @@ int main(int argc, char **argv) {
     int num_comms = 2;
     int triple_type = D3CS;
     int cut_type = D3C_COND;
-    std::string name = "flow";
+    std::string name = "small_flow";
 
     std::cout << "DIRLAP ---------" << std::endl;
     {
 	Network net = Network("data", name, triple_type);
 	CommDetection comm(net, num_comms, DIRLAP, cut_type, name);
 	comm.Run();
-    }
-    std::cout << "LAP ---------" << std::endl;
-    {
-	Network net = Network("data", name, triple_type);
-	CommDetection comm(net, num_comms, LAP, cut_type, name);
-	comm.Run();
+	std::vector<int> comms = comm.Communities();
+	WriteVector(comms, "small_dl_comms.txt");
     }
     std::cout << "TENSOR ---------" << std::endl;
     {
 	Network net = Network("data", name, triple_type);
 	CommDetection comm(net, num_comms, TENSOR, cut_type, name);
 	comm.Run();
+	std::vector<int> comms = comm.Communities();
+	WriteVector(comms, "small_tsc_comms.txt");
     }
     std::cout << "END ---------" << std::endl;
 }
